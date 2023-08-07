91db238f85f17eb3f3e83d8a9042dbb0edfa6ec5
rohitmalvi1998 <rohitmalvi@23gmail.com>
2023-08-08 00:40:33 +0530

    commit teamReport

--- arrayPractices.js
+++ arrayPractices.js
@@ -1,8 +1,32 @@
-let arrayPractice=[1,2,3,3,4,5]
+// let arrayPractice=[1,2,3,3,4,5]
+
+
+// console.log(arrayPractice[1]);
+
+// var element=arrayPractice.splice(1,4)
+// console.log(arrayPractice);
 //arrayPractice.pop() //pop out last element from array
 //arrayPractice.shift()//pop out first element of the array
 //arrayPractice.push(20)//push element in the  last array
 //arrayPractice.unshift(70)
-let arrayP= arrayPractice.slice(2,4);
-console.log(arrayP);//push element in the  first array
+//let arrayP= arrayPractice.slice(2,4);
+///console.log(arrayP);//push element in the  first array
 //arrayPractice.slice(4)
+ let array2=[7,0,8,5,10];
+console.log(...array2);
+ let length=array2.length;
+ 
+// array2[0]=largest;
+
+ 
+//  for (let i = 0; i < array2.length; i++) {
+//     for (let j = 0; j < array2.length; j++) {
+
+//     if (array2[i]>array2[j]) {
+//         const temp = array2[i];
+//         array2[i] = array2[j];
+//         array2[j]=temp;
+//     }
+//     }   
+// }
+// console.log(array2);
